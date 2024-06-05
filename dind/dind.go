b95e114ece293da1d4fff7d57e7601da979383c8
vardhaman22 <vardhaman.surana@suse.com>
2024-06-05 06:57:52 +0530

    fix

--- dind/dind.go
+++ dind/dind.go
@@ -3,6 +3,8 @@ package dind
 import (
 	"context"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
@@ -108,27 +110,83 @@ func StartUpDindContainer(ctx context.Context, dindAddress, dindNetwork, dindSto
 }
 
 func RmoveDindContainer(ctx context.Context, dindAddress string) error {
-	cli, err := client.NewEnvClient()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithTimeout(60*time.Second))
 	if err != nil {
 		return err
 	}
 	containerName := fmt.Sprintf("%s-%s", DINDContainerPrefix, dindAddress)
 	logrus.Infof("[%s] Removing dind container [%s] on host [%s]", DINDPlane, containerName, cli.DaemonHost())
-	_, err = cli.ContainerInspect(ctx, containerName)
-	if err != nil {
-		if !client.IsErrNotFound(err) {
-			return nil
+
+	retry := 0
+	var errInspecting error
+	containerInspected := false
+
+	for retry < 4 && errInspecting == nil && !containerInspected {
+		ct, cancel := context.WithTimeout(ctx, 60*time.Second)
+		defer cancel()
+		_, err = cli.ContainerInspect(ct, containerName)
+		if err != nil {
+			if !client.IsErrNotFound(err) {
+				logrus.Infof("container %s not found while inspecting", containerName)
+				return nil
+			} else if IsTimeoutError(err) {
+				logrus.Infof("timeout while inspecting container %s......retrying", containerName)
+				retry++
+				continue
+			} else {
+				errInspecting = err
+				break
+			}
 		}
+		containerInspected = true
 	}
-	if err := cli.ContainerRemove(ctx, containerName, types.ContainerRemoveOptions{
-		Force:         true,
-		RemoveVolumes: true}); err != nil {
-		if client.IsErrNotFound(err) {
-			logrus.Debugf("[remove/%s] Container doesn't exist on host [%s]", containerName, cli.DaemonHost())
-			return nil
+
+	if errInspecting != nil {
+		return err
+	} else if retry >= 4 {
+		return fmt.Errorf("not able remove [%s] container after 4 retries....", containerName)
+	}
+
+	retry = 0
+	var errRemoving error
+	containerRemoved := false
+	for retry < 4 && errRemoving == nil && !containerRemoved {
+		ct, cancel := context.WithTimeout(ctx, 60*time.Second)
+		defer cancel()
+		if err := cli.ContainerRemove(ct, containerName, types.ContainerRemoveOptions{
+			Force:         true,
+			RemoveVolumes: true}); err != nil {
+			if client.IsErrNotFound(err) {
+				logrus.Infof("[remove/%s] Container doesn't exist on host [%s]", containerName, cli.DaemonHost())
+				containerRemoved = true
+				continue
+
+			} else if IsTimeoutError(err) {
+				logrus.Infof("[remove/%s] Timeout while removing Container on host [%s]...retrying ,err: %v", containerName, cli.DaemonHost(), err)
+				retry++
+				continue
+			}
+			errRemoving = fmt.Errorf("Failed to remove dind container [%s] on host [%s]: %v", containerName, cli.DaemonHost(), err)
+			continue
 		}
-		return fmt.Errorf("Failed to remove dind container [%s] on host [%s]: %v", containerName, cli.DaemonHost(), err)
+		containerRemoved = true
+		logrus.Infof("[remove/%s] Container successfully removed  on host [%s]", containerName, cli.DaemonHost())
 	}
+	if containerRemoved {
+		return nil
+	} else if errRemoving != nil {
+		return errRemoving
+	} else if retry >= 4 {
+		return fmt.Errorf("not able remove [%s] container after 4 retries....", containerName)
+	}
+
 	logrus.Infof("[%s] Successfully Removed dind container [%s] on host [%s]", DINDPlane, containerName, cli.DaemonHost())
 	return nil
+}
+
+func IsTimeoutError(err error) bool {
+	if err != nil && (strings.Contains(strings.ToLower(err.Error()), "timeout") || strings.Contains(strings.ToLower(err.Error()), "deadline")) {
+		return true
+	}
+	return false
 }
